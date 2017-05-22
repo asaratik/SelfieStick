7d9ba729fcfb8a96d0365864f10ad6fd236f2f3b
SAMEERA PRIYATHAM TADIKONDA <priyatham@Priyathams-MacBook-Pro.local>
2017-05-21 17:07:58 -0700

    name in posts

--- routes/index.js
+++ routes/index.js
@@ -22,15 +22,41 @@ router.get('/',function(req,res){
 				data: returnvalue
 			});
 		});*/
-	var myPosts;		
-		db.posts.find( {user_id: req.user.uuid}).sort({priority:1},function(err,returnvalue){
-	if(err){
-		return console.dir(err);
-	}
-	console.log("My posts: "+JSON.stringify(returnvalue));
-	res.render('index', { user : req.user, data : returnvalue});});
+	                db.users.find({
+                    _id: req.user._id
+                }).sort({
+                    priority: 1
+                }, function(err, returnvalue) {
+                    if (err) {
+                        return console.dir(err);
+                    }
+                    var followers = returnvalue[0].circle;
+                    
+                    //for(follower in followers){
+                    //  console.log(followers[follower]);
+                    //  abc = "ObjectId('"+followers[follower]+"')";
+                    //  arr.push("ObjectId('"+followers[follower]+"')");    
+                    //  //var obj= {"$oid": followers[follower]};
+                    //  //arr.push(obj);
+                    //}
+                    //console.log("FOlooooooooooooooooo---- "+ JSON.stringify(arr));
+                    //console.log("Cooooooooooo---"+ JSON.stringify(returnvalue[0].circle));
+                    //db.users.find( { _id: { $nin: returnvalue[0].circle } } )
+                    //db.users.find({_id: req.user._id}).sort({priority:1}
+                    db.posts.find( {user_id: {$in : followers}},function(err,returnvalu){
+				if(err){
+					return console.dir(err);
+				}
+				console.log("My posts: "+JSON.stringify(returnvalu));
+					res.render('index', { user : req.user, data : returnvalu});});
 	//var followers = returnvalue[0].circle;
+                });
+
+
+
 	
+	//var followers = returnvalue[0].circle;
+
 
 		}else{
 			  res.redirect('/users/login');
